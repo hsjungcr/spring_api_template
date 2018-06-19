33add4f4b31c0c941a5cd565b9781a88b0244802
Han Jung <han.jung@finra.org>
2018-06-19 13:24:55 -0400

    initial commit

---
+++ src/main/java/org/jungdev/Application.java
@@ -0,0 +1,10 @@
+package org.jungdev;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        System.out.println("Application is Running...");
+    }
+}
